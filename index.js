7afd77083ee1d495e41a73897766c6f14ac2e5b2
m-h-thomas <mhthomas.8193@gmail.com>
2025-02-05 20:16:46 -0700

    index.js button now adds input to notStartedContainer, small style.css update

--- index.js
+++ index.js
@@ -1,23 +1,50 @@
-console.log('My code is running.'); //inital test to confirm link to html file. --it works--
+//console.log('My code is running.'); //inital test to confirm link to html file. --it works--
 
 /* Global variables to hold user inputs and all tasks array */
 let allTasks = [];
 let allInputs = '';
 
-let addBtn = document.getElementsByClassName('btn'); //assign var addBtn to HTML button element
 
-for (i = 0; i < addBtn.length; i++) {
-  let button = addBtn[i]
-  button.addEventListener('click', function(event) {
-    console.log('button clicked!')
-    let buttonClicked = event.target
-    buttonClicked
-    // addNewTask()
+/* Function to take user inputs and add new task to notStarted container */
+const addNewTask = () => {
+  let newTask = document.querySelector('#input-box');
+  let addTask = document.querySelector('#add-task')
+
+  newTask.addEventListener('input', (eventObj) => {
+    allInputs = eventObj.target.value
+  })
+
+  addTask.addEventListener('click', () => {
+    allTasks.push(allInputs);
+    newTask.value = '';
+    display();
   })
 }
 
+/* Dynamically displays items from allTasks array */
+const display = () => {
+  let notStartedTasks = document.querySelector('.notStartedContainer');
+  let listTasks = allTasks.map((elm) => `<div><button class="item">${elm}</button><button class='x'>x</button></div>`)
+  .reduce((acc, elm) => acc += elm, '');
+  notStartedTasks.innerHTML = listTasks;
+};
 
-/* Function to take user inputs and add new task to notStarted container */
-// const function addNewTask () {
-//   let newTask = ;
-// }
+addNewTask();
+
+// /* On button click, grabs input box value, adds to allItems array, and displays */
+// const getInputValue = () => {
+//   let inputBox = document.querySelector('#input-box');
+//   let addBtn = document.querySelector('#add-item-button');
+
+//   inputBox.addEventListener('input', (eventObj) => {
+//     userInput = eventObj.target.value;
+//   });
+
+//   addBtn.addEventListener('click', () => {
+//     allItems.push(userInput);
+//     inputBox.value = '';
+//     displayItems();
+//   });
+// };
+
+// getInputValue();
